5cf290dffbe6fe744312fc8e0db7e548d3036e7f
Rémy Woine <remy.woine@gmail.com>
2016-06-01 12:58:28 +0200

    Fill up TCP Server functions

--- WLink/TCPServer.cpp
+++ WLink/TCPServer.cpp
@@ -16,6 +16,7 @@
 /* ******************************************************************************** */
 
 #include "TCPServer.h"
+#include "Hardware.h"
 
 #include "Debug.h"
 
@@ -27,7 +28,10 @@
 /* Constructor
 /* ******************************************************************************** */
 TCPServer::TCPServer() {
-
+	GL_TcpServerParam_X.IsInitialized_B = false;
+	GL_TcpServerParam_X.IsConnected_B = false;
+	GL_TcpServerParam_X.Status_E = TCP_SERVER_IDLE;
+	pinMode(PIN_ETH_LINKED, INPUT);
 }
 
 
@@ -35,63 +39,136 @@ TCPServer::TCPServer() {
 /* Functions
 /* ******************************************************************************** */
 void TCPServer::init() {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = TCP_SERVER_DEFAULT_MAC_ADDR0;
+	GL_TcpServerParam_X.pMacAddr_UB[1] = TCP_SERVER_DEFAULT_MAC_ADDR1;
+	GL_TcpServerParam_X.pMacAddr_UB[2] = TCP_SERVER_DEFAULT_MAC_ADDR2;
+	GL_TcpServerParam_X.pMacAddr_UB[3] = TCP_SERVER_DEFAULT_MAC_ADDR3;
+	GL_TcpServerParam_X.pMacAddr_UB[4] = TCP_SERVER_DEFAULT_MAC_ADDR4;
+	GL_TcpServerParam_X.pMacAddr_UB[5] = TCP_SERVER_DEFAULT_MAC_ADDR5;
+	GL_TcpServerParam_X.LocalPort_UI = TCP_SERVER_DEFAULT_PORT;
+	GL_TcpServerParam_X.IsDhcp_B = true;
+	GL_TcpServerParam_X.IsInitialized_B = true;
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Initialized");
 }
 
 void TCPServer::init(unsigned char pMacAddr_UB[6]) {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = pMacAddr_UB[0];
+	GL_TcpServerParam_X.pMacAddr_UB[1] = pMacAddr_UB[1];
+	GL_TcpServerParam_X.pMacAddr_UB[2] = pMacAddr_UB[2];
+	GL_TcpServerParam_X.pMacAddr_UB[3] = pMacAddr_UB[3];
+	GL_TcpServerParam_X.pMacAddr_UB[4] = pMacAddr_UB[4];
+	GL_TcpServerParam_X.pMacAddr_UB[5] = pMacAddr_UB[5];
+	GL_TcpServerParam_X.LocalPort_UI = TCP_SERVER_DEFAULT_PORT;
+	GL_TcpServerParam_X.IsDhcp_B = true;
+	GL_TcpServerParam_X.IsInitialized_B = true;
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Initialized");
 }
 
 void TCPServer::init(unsigned char pMacAddr_UB[6], IPAddress IpAddr_X) {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = pMacAddr_UB[0];
+	GL_TcpServerParam_X.pMacAddr_UB[1] = pMacAddr_UB[1];
+	GL_TcpServerParam_X.pMacAddr_UB[2] = pMacAddr_UB[2];
+	GL_TcpServerParam_X.pMacAddr_UB[3] = pMacAddr_UB[3];
+	GL_TcpServerParam_X.pMacAddr_UB[4] = pMacAddr_UB[4];
+	GL_TcpServerParam_X.pMacAddr_UB[5] = pMacAddr_UB[5];
+	GL_TcpServerParam_X.IpAddr_X = IpAddr_X;
+	GL_TcpServerParam_X.LocalPort_UI = TCP_SERVER_DEFAULT_PORT;
+	GL_TcpServerParam_X.IsDhcp_B = false;
+	GL_TcpServerParam_X.IsInitialized_B = true;
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Initialized");
 }
 
 void TCPServer::init(unsigned char pMacAddr_UB[6], IPAddress IpAddr_X, unsigned int LocalPort_UI) {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = pMacAddr_UB[0];
+	GL_TcpServerParam_X.pMacAddr_UB[1] = pMacAddr_UB[1];
+	GL_TcpServerParam_X.pMacAddr_UB[2] = pMacAddr_UB[2];
+	GL_TcpServerParam_X.pMacAddr_UB[3] = pMacAddr_UB[3];
+	GL_TcpServerParam_X.pMacAddr_UB[4] = pMacAddr_UB[4];
+	GL_TcpServerParam_X.pMacAddr_UB[5] = pMacAddr_UB[5];
+	GL_TcpServerParam_X.IpAddr_X = IpAddr_X;
+	GL_TcpServerParam_X.LocalPort_UI = LocalPort_UI;
+	GL_TcpServerParam_X.IsDhcp_B = false;
+	GL_TcpServerParam_X.IsInitialized_B = true;
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Initialized");
 }
 
 void TCPServer::init(unsigned char pMacAddr_UB[6], unsigned int LocalPort_UI) {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = pMacAddr_UB[0];
+	GL_TcpServerParam_X.pMacAddr_UB[1] = pMacAddr_UB[1];
+	GL_TcpServerParam_X.pMacAddr_UB[2] = pMacAddr_UB[2];
+	GL_TcpServerParam_X.pMacAddr_UB[3] = pMacAddr_UB[3];
+	GL_TcpServerParam_X.pMacAddr_UB[4] = pMacAddr_UB[4];
+	GL_TcpServerParam_X.pMacAddr_UB[5] = pMacAddr_UB[5];
+	GL_TcpServerParam_X.LocalPort_UI = LocalPort_UI;
+	GL_TcpServerParam_X.IsDhcp_B = true;
+	GL_TcpServerParam_X.IsInitialized_B = true;
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Initialized");
 }
 
 boolean TCPServer::isInitialized() {
-
+	return GL_TcpServerParam_X.IsInitialized_B;
 }
 
 
 void TCPServer::setMacAddr(unsigned char pMacAddr_UB[6]) {
-
+	GL_TcpServerParam_X.pMacAddr_UB[0] = pMacAddr_UB[0];
+	GL_TcpServerParam_X.pMacAddr_UB[1] = pMacAddr_UB[1];
+	GL_TcpServerParam_X.pMacAddr_UB[2] = pMacAddr_UB[2];
+	GL_TcpServerParam_X.pMacAddr_UB[3] = pMacAddr_UB[3];
+	GL_TcpServerParam_X.pMacAddr_UB[4] = pMacAddr_UB[4];
+	GL_TcpServerParam_X.pMacAddr_UB[5] = pMacAddr_UB[5];
 }
 
 void TCPServer::setIpAddr(IPAddress IpAddr_X) {
-
+	GL_TcpServerParam_X.IsDhcp_B = false;
+	GL_TcpServerParam_X.IpAddr_X = IpAddr_X;
 }
 
 void TCPServer::setLocalPort(unsigned int LocalPort_UI) {
-
+	GL_TcpServerParam_X.LocalPort_UI = LocalPort_UI;
 }
 
 unsigned char * TCPServer::getMacAddr() {
-
+	return GL_TcpServerParam_X.pMacAddr_UB;
 }
 
 IPAddress TCPServer::getIpAddr() {
-
+	return GL_TcpServerParam_X.IpAddr_X;
 }
 
 unsigned int TCPServer::getLocalPort() {
-
+	return GL_TcpServerParam_X.LocalPort_UI;
 }
 
 
 void TCPServer::begin() {
+	if (GL_TcpServerParam_X.IsDhcp_B) {
+		Ethernet.begin(GL_TcpServerParam_X.pMacAddr_UB);
+		GL_TcpServerParam_X.IpAddr_X = Ethernet.localIP();
+	}
+	else {
+		Ethernet.begin(GL_TcpServerParam_X.pMacAddr_UB, GL_TcpServerParam_X.IpAddr_X);
+	}
 
+	GL_TcpServerParam_X.Server_H = EthernetServer(GL_TcpServerParam_X.LocalPort_UI);
+	GL_TcpServerParam_X.Server_H.begin();
+	GL_TcpServerParam_X.IsConnected_B = true;
+
+	DBG_PRINTLN(DEBUG_SEVERITY_INFO, "TCP Server Started @ ");
+	DBG_PRINTDATA(GL_TcpServerParam_X.IpAddr_X);
+	DBG_PRINTDATA(":");
+	DBG_PRINTDATA(GL_TcpServerParam_X.LocalPort_UI);
+	DBG_ENDSTR();
 }
 
 void TCPServer::renew() {
-
+	GL_TcpServerParam_X.IsConnected_B = false;
+	GL_TcpServerParam_X.Client_H.flush(); 	// Flush buffer
+	GL_TcpServerParam_X.Client_H.stop(); 	// Close connection if any
+	GL_TcpServerParam_X.Status_E = TCP_SERVER_IDLE;
+	begin();
 }
 
 boolean TCPServer::isEthernetLinked() {
-
+	return ((digitalRead(PIN_ETH_LINKED) == LOW) ? true : false);
 }
