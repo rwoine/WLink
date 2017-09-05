df66880018816e88fd61230c3f90f3392fbe2459
Rémy Woine <remy.woine@gmail.com>
2017-09-05 22:03:58 +0200

    Change polling statuses in FonaModule.

--- WLink/FonaModuleManager.cpp
+++ WLink/FonaModuleManager.cpp
@@ -27,7 +27,7 @@
 #define FONA_MODULE_MANAGER_POWER_PULSE_LENGTH_MS				2000
 #define FONA_MODULE_MANAGER_RESET_PULSE_LENGTH_MS				100
 #define FONA_MODULE_MANAGER_NETWORK_REGISTRATION_TIMEOUT_MS		20000
-#define FONA_MODULE_MANAGER_POLLING_INTERVAL_MS					5000
+#define FONA_MODULE_MANAGER_POLLING_INTERVAL_MS					2000
 
 
 /* ******************************************************************************** */
@@ -60,6 +60,7 @@ static unsigned long GL_FonaManagerPowerSequenceNb_UL = 0;
 static unsigned long GL_FonaAbsoluteTime_UL = 0;
 static boolean GL_FonaModuleManagerEnabled_B = false;
 static boolean GL_FonaModuleManagerEnableGprs_B = false;
+static unsigned long GL_FonaModuleManagerPollingIndex_UL = 0;
 
 
 /* ******************************************************************************** */
@@ -285,10 +286,16 @@ void ProcessRunning(void) {
 
     // Poll Statuses
     if ((millis() - GL_FonaAbsoluteTime_UL) >= FONA_MODULE_MANAGER_POLLING_INTERVAL_MS) {
-        
-        GL_FonaModuleManagerRssi_SI = GL_pFona_H->getSignalStrength();
-        GL_FonaModuleManagerGprsState_B = GL_pFona_H->isGprsEnabled();
-		GL_FonaModuleManagerBatteryLeve_UI = GL_pFona_H->getBatteryLevel();
+
+		switch (GL_FonaModuleManagerPollingIndex_UL) {
+		case 0:	GL_FonaModuleManagerRssi_SI = GL_pFona_H->getSignalStrength();		break;
+		case 1:	GL_FonaModuleManagerGprsState_B = GL_pFona_H->isGprsEnabled();		break;
+		case 2:	GL_FonaModuleManagerBatteryLeve_UI = GL_pFona_H->getBatteryLevel();	break;
+		default:	break;
+		}
+
+		GL_FonaModuleManagerPollingIndex_UL++;
+		GL_FonaModuleManagerPollingIndex_UL = GL_FonaModuleManagerPollingIndex_UL % 3;
 
         GL_FonaAbsoluteTime_UL = millis();
     }
